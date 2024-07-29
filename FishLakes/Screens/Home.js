96117a8c19e8e70366f2352e1f5d219d04e95af2
RonaldPS1993 <ronaldpenasollet@gmail.com>
2024-07-29 01:05:35 -0400

    Responsive functions created

--- FishLakes/Screens/Home.js
+++ FishLakes/Screens/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   View,
   Dimensions,
@@ -7,22 +7,80 @@ import {
   StyleSheet,
   Text,
 } from "react-native";
+import MapView from "react-native-maps";
+import * as Location from "expo-location";
+import {
+  responsiveModerateScale,
+  responsiveVerticalScale,
+  responsiveScale,
+} from "../lib/sizeFunctions";
+
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 
 const Home = () => {
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [region, setRegion] = useState();
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let position = await Location.getCurrentPositionAsync({});
+
+      setRegion({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+        latitudeDelta: 0.02,
+        longitudeDelta: 0.02,
+      });
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>MapView</Text>
+      <MapView style={styles.map} region={region}>
+        <View style={styles.card}></View>
+      </MapView>
     </View>
   );
 };
 
-export default Home
+export default Home;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    width: "100%",
+    height: "100%",
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+    flex: 1,
+    alignItems: "center",
+    flexDirection: "column",
+    justifyContent: "flex-end"
+  },
+  card: {
+    width: responsiveScale(300),
+    height: responsiveVerticalScale(150),
+    backgroundColor: "white",
+    borderRadius: responsiveModerateScale(10),
+    shadowColor: "rgba(109,109,109,1)",
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
+    elevation: 20,
+    shadowOpacity: 0.2,
+    shadowRadius: 10,
+    bottom: responsiveModerateScale(50)
   },
 });
